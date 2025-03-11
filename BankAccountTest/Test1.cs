777fd39abdca3734d48e9ae14cb5e933f10340c7
Josiah Bernard <josiahrbernard@gmail.com>
2025-03-11 15:30:11 -0400

    initiilze

---
+++ BankAccountTest/Test1.cs
@@ -0,0 +1,26 @@
+﻿using Ban
+namespace BankAccountTest;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void Debit_WithValidAmount()
+    {
+        // Arrange
+        double startingBalance = 11.99;
+        double debitAmmount = 4.55;
+        double expected = 7.44;
+
+        BankAccount account = new BankAccount("Mr. Bryan Walton", startingBalance);
+
+        // Act
+        account.Debit(debitAmmount);
+
+
+        // Assert
+        double actual 
+
+
+    }
+}
